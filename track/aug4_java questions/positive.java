2bf9643b01613137a96db03a8b4ce97f74093624
9922008112 <9922008112@klu.ac.in>
2026-08-04 12:27:57 +0530

    if number is positive

---
+++ track/aug4_java questions/positive.java
@@ -0,0 +1,8 @@
+public class positive{
+    public static void main (String[] args){
+        int num = 25;
+        if(num > 0){
+            System.out.println("The number is positive");
+        }
+    }
+}
