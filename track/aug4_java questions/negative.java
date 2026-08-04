4444da5c64774bb8e2d1156bfe5480a7d3aeb5e6
9922008112 <9922008112@klu.ac.in>
2026-08-04 12:33:05 +0530

    If nuber is negative

---
+++ track/aug4_java questions/negative.java
@@ -0,0 +1,11 @@
+public class negative{
+    public static void main(String[] args){
+        int num = -25;
+        if (num > 0){
+            System.out.println("The number is positive");
+
+        }else{
+            System.out.println("The number is negative");
+        }
+    }
+}
