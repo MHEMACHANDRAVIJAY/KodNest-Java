79811cec9a7fee1e8d97cfc82bf2ee1429371605
9922008112 <9922008112@klu.ac.in>
2026-08-06 11:42:35 +0530

    Nestedloop dowhile concept

---
+++ track/M2_Introduction to java/NestedLoop3.java
@@ -0,0 +1,18 @@
+public class NestedLoop3 {
+    public static void main(string[] args) {
+        int i= 1;
+        do{
+            int j= 1;
+            do{
+                System.out.println(j);
+                j++;
+            } while(j<=5);
+
+            System.out.println();
+            i++
+        }while(i<=5);
+
+
+    }
+
+}
